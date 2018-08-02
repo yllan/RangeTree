50d079267d120d21f82eee331d98afa1d83a62c4
Yung-Luen Lan <yllan@me.com>
2018-08-02 14:46:54 +0800

    Initial Commit

---
+++ RangeTree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RangeTree
+//
+//  Created by Yung-Luen Lan on 2018/8/2.
+//  Copyright © 2018 yllan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
