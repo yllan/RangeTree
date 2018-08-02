04fbe3d9ec12df58b02d8a0f5eedf2e09fa488fc
Yung-Luen Lan <yllan@me.com>
2018-08-02 16:53:42 +0800

    1D binary search

---
+++ RangeTree/Array+BinarySearch.swift
@@ -0,0 +1,31 @@
+//
+//  Array+BinarySearch.swift
+//  RangeTree
+//
+//  Created by Yung-Luen Lan on 2018/8/2.
+//  Copyright © 2018 yllan. All rights reserved.
+//
+
+extension Array where Element: Comparable {
+    // Assuming the array is ordered like this: [F, F, F, ... T, T, T]
+    // This function find the index of first element evaluates to true.
+    // Time complexity: O(lgn)
+    func indexOfFirst(where f: (Element) -> Bool) -> Index {
+        guard let last = self.last, f(last) else {
+            return self.count
+        }
+        var idx: Int = 0
+        var len: Int = self.count
+        while len > 0 {
+            let half = (len >> 1)
+            let m = idx + half
+            if f(self[m]) {
+                len = half
+            } else {
+                idx = m + 1
+                len = len - half - 1
+            }
+        }
+        return idx
+    }
+}
--- RangeTree/main.swift
+++ RangeTree/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+class RangeSearch1D<T: Comparable> {
+    let sortedPoints: [T]
+    init(points: [T]) {
+        sortedPoints = points.sorted()
+    }
+    
+    func query(lower: T, upper: T) -> ArraySlice<T> {
+        let l = sortedPoints.indexOfFirst(where: { $0 >= lower })
+        let r = sortedPoints.indexOfFirst(where: { $0 > upper })
+        return sortedPoints[l..<r]
+    }
+}
 
+let points: [CGFloat] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1]
+let oneDimensionSearcher = RangeSearch1D(points: points)
+print(oneDimensionSearcher.query(lower: 1.2, upper: 2.05))
