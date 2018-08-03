efff13f8264d4a09b11749b4e481120a83f7d14d
Yung-Luen Lan <yllan@me.com>
2018-08-03 17:45:44 +0800

    Remove range in node

--- RangeTree/RangeSearch2D.swift
+++ RangeTree/RangeSearch2D.swift
@@ -191,13 +191,13 @@ class CrossLinking2DRangeTree {
     
     indirect enum Tree {
         case Empty
-        case Node(leftTree: Tree, rightTree: Tree, range: CountableRange<Int>, sortedByY: [CGPoint], links: [Link])
+        case Node(leftTree: Tree, rightTree: Tree, sortedByY: [CGPoint], links: [Link])
         
         static func build(range: CountableRange<Int>, array: [CGPoint]) -> Tree {
             if range.count == 0 {
                 return Tree.Empty
             } else if range.count == 1 {
-                return Tree.Node(leftTree: .Empty, rightTree: .Empty, range: range, sortedByY: [array[range.startIndex]], links: [])
+                return Tree.Node(leftTree: .Empty, rightTree: .Empty, sortedByY: [array[range.startIndex]], links: [])
             } else {
                 let lRange = range.prefix(range.count / 2)
                 let rRange = range.suffix(from: lRange.endIndex)
@@ -209,7 +209,7 @@ class CrossLinking2DRangeTree {
                 var sortedByY: [CGPoint] = []
                 var links: [Link] = []
                 switch (lTree, rTree) {
-                case let (.Node(_, _, _, lPoints, _), .Node(_, _, _, rPoints, _)):
+                case let (.Node(_, _, lPoints, _), .Node(_, _, rPoints, _)):
                     var lIdx = 0, rIdx = 0, idx = 0
                     sortedByY = Array<CGPoint>(repeating: .zero, count: lPoints.count + rPoints.count)
                     links = Array<Link>(repeating: (0, 0), count: lPoints.count + rPoints.count)
@@ -231,7 +231,7 @@ class CrossLinking2DRangeTree {
                     break
                 }
                 
-                return Tree.Node(leftTree: lTree, rightTree: rTree, range: range, sortedByY: sortedByY, links: links)
+                return Tree.Node(leftTree: lTree, rightTree: rTree, sortedByY: sortedByY, links: links)
             }
         }
     }
@@ -248,14 +248,14 @@ class CrossLinking2DRangeTree {
         let l = sortedByX.indexOfFirst(where: { $0.x >= rect.minX })
         let r = sortedByX.indexOfFirst(where: { $0.x > rect.maxX })
         
-        func searchTree(_ t: Tree, searchRange: CountableRange<Int>, yRange: CountableRange<Int>) -> [CGPoint] {
+        func searchTree(_ t: Tree, nodeRange: CountableRange<Int>, searchRange: CountableRange<Int>, yRange: CountableRange<Int>) -> [CGPoint] {
             switch t {
             case .Empty:
                 return []
-            case let .Node(leftTree, rightTree, range, sortedByY, links):
-                let trimmedSearchRange = searchRange.clamped(to: range)
+            case let .Node(leftTree, rightTree, sortedByY, links):
+                let trimmedSearchRange = searchRange.clamped(to: nodeRange)
                 
-                if trimmedSearchRange == range {
+                if trimmedSearchRange == nodeRange {
                     return Array(sortedByY[yRange])
                 } else if !trimmedSearchRange.isEmpty {
                     guard yRange.startIndex < sortedByY.count else {
@@ -266,7 +266,7 @@ class CrossLinking2DRangeTree {
                     var rightYRange = 0..<0
                     
                     switch (leftTree, rightTree) {
-                    case let (.Node(_, _, _, leftY, _), .Node(_, _, _, rightY, _)):
+                    case let (.Node(_, _, leftY, _), .Node(_, _, rightY, _)):
                         leftYRange = Int(links[yRange.startIndex].leftIndex)..<(
                                         yRange.endIndex == sortedByY.count ?
                                         leftY.count :
@@ -280,7 +280,10 @@ class CrossLinking2DRangeTree {
                     default:
                         fatalError("Not good!")
                     }
-                    return searchTree(leftTree, searchRange: trimmedSearchRange, yRange: leftYRange) + searchTree(rightTree, searchRange: trimmedSearchRange, yRange: rightYRange)
+                    let lRange = nodeRange.prefix(nodeRange.count / 2)
+                    let rRange = nodeRange.suffix(from: lRange.endIndex)
+                    
+                    return searchTree(leftTree, nodeRange: lRange, searchRange: trimmedSearchRange, yRange: leftYRange) + searchTree(rightTree, nodeRange: rRange, searchRange: trimmedSearchRange, yRange: rightYRange)
                 } else { // no overlap
                     return []
                 }
@@ -288,10 +291,10 @@ class CrossLinking2DRangeTree {
         }
         
         switch tree {
-            case let .Node(_, _, _, sortedByY, _):
+            case let .Node(_, _, sortedByY, _):
                 let bottom = sortedByY.indexOfFirst(where: { $0.y >= rect.minY })
                 let top = sortedByY.indexOfFirst(where: { $0.y > rect.maxY })
-                return searchTree(tree, searchRange: l..<r, yRange: bottom..<top)
+                return searchTree(tree, nodeRange: 0..<sortedByX.count, searchRange: l..<r, yRange: bottom..<top)
             default:
                 return []
         }
